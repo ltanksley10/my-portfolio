7be7180c3a26e41ba20691fef1bd7f834f04bf97
ltanksley10 <contactlthere@gmail.com>
2021-05-11 15:40:10 +0000

    updates to swiper script and added arrow images

--- scripts/script.js
+++ scripts/script.js
@@ -2,47 +2,43 @@ $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
 
-document.querySelector('#scroll-to-resume-link').addEventListener('click', function() {
-    document.querySelector('#resume').scrollIntoView();
-});
-          
 document.querySelector('#scroll-to-projects').addEventListener('click', function() {
     document.querySelector('#projects').scrollIntoView();
 });
 
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    grabCursor: true,
+    mousewheel: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          slidesPerGroup: 3
+        },
+        768: {
             slidesPerView: 2,
-            spaceBetween: 30,
-            slidesPerGroup: 2,
-            loop: true,
-            loopFillGroupWithBlank: true,
-            grabCursor: true,
-            mousewheel: false,
-            pagination: {
-              el: '.swiper-pagination',
-              clickable: true,
-            },
-            autoplay: {
-                delay: 3000,
-                disableOnInteraction: true,
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-            breakpoints: {
-                // 1200: {
-                //   slidesPerView: 3,
-                //   slidesPerGroup: 3
-                // },
-                1024: {
-                    slidesPerView: 2,
-                    slidesPerGroup: 2
-                },
-                320: {
-                  slidesPerView: 1,
-                  slidesPerGroup: 1
-                }
-                
-            }
-    });
+            slidesPerGroup: 2
+        },
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1
+        }
+        
+    }
+});
